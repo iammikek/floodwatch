773915d139c817d42683c6e9fa35ecc59b73d42e
mike karthauser <mike@automica.io>
2026-03-18 19:14:28 +0000

    Tests: use markTestSkipped when Pulse is not installed

--- tests/Feature/PulseAccessTest.php
+++ tests/Feature/PulseAccessTest.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Config;
 
 beforeEach(function () {
     if (! class_exists('Laravel\\Pulse\\PulseServiceProvider')) {
-        test()->skip('Pulse not installed for this environment');
+        $this->markTestSkipped('Pulse not installed for this environment');
     }
 
     Config::set('pulse.enabled', true);
